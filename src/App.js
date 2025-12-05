46597763d54f7e0b4791037b72ac7f2303ae3331
Rajesh Kumar <rajesh@example.com>
2025-12-06 00:00:07 +0530

    add time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const multipliers = [3, 4, 6, 12];
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="KJUh7">
+      <h3>Price Change</h3>
+
+      <input className='price' type="number" value={value} placeholder="Enter number" onChange={handleChange} />
+
+      {value &&
+        <div>
+          <div>2/{multipliers[0]}no. (4inch) - {multipliers[0] * value}</div>
+          <div>{multipliers[1]}no. (6inch) - {multipliers[1] * value}</div>
+          <div>{multipliers[2]}no. (8inch) - {multipliers[2] * value}</div>
+          <div>{multipliers[3]}no. (12inch) - {multipliers[3] * value}</div>
+
+
+
+        </div>
+      }
     </div>
   );
 }
